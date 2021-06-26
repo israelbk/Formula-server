3e23a42f13f55da462feaab5e820d8d600628eeb
Israelbk <israel727@gmail.com>
2021-06-26 19:37:23 +0300

    Adding support for targets actions

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ io.on("connection", (socket) => {
     console.log("a user connected");
 
     socket.on("disconnect", () => console.log("user disconnected"));
+
+    socket.on("action", (key) => {
+        console.log(key);
+        port.write(key);
+    });
 });
 
 http.listen(3000, () => console.log("listening on *:3000"));
